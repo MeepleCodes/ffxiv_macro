658e202d40da56141ad91581a7ecbe1f4288dcb1
Meeple <awful.meeple@gmail.com>
2022-12-26 10:00:37 +0000

    UI WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import { useEffect, useRef, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import './App.css';
 import TextEditor, {HTMLTextEditorElement} from './texteditor/TextEditorReact';
 import { MacroDoc, Store } from './store/Firebase';
 import { Bytes } from 'firebase/firestore';
-import { SaveControls, FileList, StoreContextProvider } from './store/StoreControls';
+import { SaveControls, FileList, StoreContextProvider, FileName, SaveButton } from './store/StoreControls';
 import GlyphPicker from './GlyphPicker';
 
 
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
+import EditIcon from '@mui/icons-material/Edit';
 import CardActions from '@mui/material/CardActions';
+import CardHeader from '@mui/material/CardContent';
 import CardContent from '@mui/material/CardContent';
+import { Stack } from '@mui/material';
 
 // // Force an import of this otherwise webpack doesn't think it's referenced
 // require("./TextEditor");
 
 type FontSource = {
 	name: string;
+	size: string;
 	src: string;
 	request: string;
 }
@@ -24,15 +28,12 @@ const fontSources: FontSource[] = [];
 
 function importRes(requireContext: __WebpackModuleApi.RequireContext, cache = false) {
 	requireContext.keys().forEach((request) => {
-		const res = requireContext(request);
+		const src = requireContext(request);
 		if(cache) {
 				const parts = (request.startsWith("./") ? request.substring(2) : request).split(/[-.]/).slice(0,2);
 				const name = parts[0].charAt(0).toUpperCase() + parts[0].substring(1) + " " + parts[1];
-				fontSources.push({
-					name: name,
-					src: res,
-					request: request
-				});
+				const size = parts[1];
+				fontSources.push({name, size, src, request});
 		}
 		
 	});
@@ -80,25 +81,31 @@ function App() {
 	return (
 		<StoreContextProvider editor={ref}>
 			<Card>
-				
-			<div className="row">
-				<div className="col">
-					<TextEditor fontsrc={font.src} ref={ref} onUpdate={updateCursor} value={"line\n\nline"}/>
+				<CardHeader>
+					<FileName editable={true}/>
+					<SaveButton/>
+				</CardHeader>
+				<CardContent>
+						<TextEditor fontsrc={font.src} ref={ref} onUpdate={updateCursor} value={"line\n\nline"}/>
+				</CardContent>
+			<CardActions>
+				Font:
+					{fontSources.map((fontSource: FontSource) => <button title={fontSource.request} key={fontSource.request} onClick={e => setFont(fontSource)}>{fontSource.name}</button>)}
 					{cur && <div className="status row">
 						Ln {cur.cursorRow}, Col {cur.cursorCol} [{cur.cursorX}, {cur.cursorY}] {selectionText}
 					</div>}
-					<div className="row">
-						Font:
-					{fontSources.map((fontSource: FontSource) => <button title={fontSource.request} key={fontSource.request} onClick={e => setFont(fontSource)}>{fontSource.name}</button>)}
-					</div>
-				</div>
-				<GlyphPicker editorRef={ref} />
-                <div className="col"><FileList/>
-                </div>
-			</div>
-			<CardActions>
-				<SaveControls/>
+				
 			</CardActions>
+			</Card>
+			<Card>
+				<CardContent>
+					<GlyphPicker editorRef={ref} />
+				</CardContent>
+			</Card>
+			<Card>
+				<CardContent>
+					<FileList/>
+				</CardContent>
 			</Card>
 		</StoreContextProvider>
 		
