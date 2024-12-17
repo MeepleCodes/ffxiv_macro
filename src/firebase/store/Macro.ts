128625dc2b6f553d537085108b0c8073da19358f
Meeple <awful.meeple@gmail.com>
2024-12-17 20:01:17 +0000

    More refactoring

--- src/firebase/store/Macro.ts
+++ src/firebase/store/Macro.ts
@@ -1,8 +1,8 @@
 /**
  * Doc information for macro documents
  */
-import { Bytes, DocumentData, FirestoreDataConverter, QueryDocumentSnapshot, SnapshotOptions, WithFieldValue } from "firebase/firestore";
-import { Store, UserDoc } from './UserDocStore';
+import { Bytes, DocumentData, FirestoreDataConverter, PartialWithFieldValue, QueryDocumentSnapshot, SetOptions, SnapshotOptions, WithFieldValue } from "firebase/firestore";
+import { OwnFields, Store, UserDoc } from './UserDocStore';
 
 export interface MacroDoc extends UserDoc {
     name: string;
@@ -24,8 +24,25 @@ export enum MacroSortKeys {
     updated = "updated",
     name = "name"
 }
-export const macroStore = new Store<MacroDoc>(
-    "macros",
-    converter,
-    () => ({name: "", text: "", thumbnail: Bytes.fromBase64String("")})
-);
+export class MacroStore extends Store<MacroDoc> {
+    protected constructOwnFields(): OwnFields<MacroDoc> {
+        return {
+            text: "",
+            thumbnail: Bytes.fromBase64String("")
+        }
+    }
+    protected hasChangedOwnFields(a: OwnFields<MacroDoc>, b: OwnFields<MacroDoc>): boolean {
+        return a.text !== b.text;
+    }
+    fromFirestore(snapshot: QueryDocumentSnapshot, _options?: SnapshotOptions): MacroDoc {
+        const {owner, name, text, created, updated, thumbnail, deleted} = snapshot.data();
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        return {id: snapshot.id, owner, name, text, created, updated, thumbnail, deleted};
+    }
+    toFirestore(macro: WithFieldValue<MacroDoc>): DocumentData {
+        const {owner, name, text, created, updated, thumbnail, deleted} = macro;
+        return {owner, name, text, created, updated, thumbnail, deleted};
+    }
+}
+
+export const macroStore = new MacroStore("macros");
